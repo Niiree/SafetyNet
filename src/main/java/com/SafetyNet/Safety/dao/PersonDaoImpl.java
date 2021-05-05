12b5cec57d1cb9b074255fcb7401a12e42eed88f
Nicolas.le-stunff <nicolas.le-stunff@hotmail.fr>
2021-05-05 21:47:49 +0200

    Implémentation du PersonDao

---
+++ src/main/java/com/SafetyNet/Safety/dao/PersonDaoImpl.java
@@ -0,0 +1,37 @@
+package com.SafetyNet.Safety.dao;
+
+import com.SafetyNet.Safety.model.Person;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class PersonDaoImpl implements PersonDao {
+
+    public static List<Person>persons = new ArrayList<>();
+
+    static {
+        persons.add(new Person(1,"Nicolas","Le stunff","InfoAddresse","Ville","Zip","phone","email@email.com"));
+    }
+
+    @Override
+    public List<Person> findAll() {
+        return persons;
+    }
+
+    @Override
+    public Person PersonfindById(int id) {
+        for(Person person : persons){
+            if(person.getId() == id){
+                return person;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void PersonSave(Person person) {
+        persons.add(person);
+    }
+}
