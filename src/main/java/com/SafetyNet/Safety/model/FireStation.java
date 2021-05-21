ffd55cc379a0959f1ecd9819f6bf57a8f57c6736
Nicolas.le-stunff <nicolas.le-stunff@hotmail.fr>
2021-05-21 17:30:36 +0200

    Ajout FireStation

---
+++ src/main/java/com/SafetyNet/Safety/model/FireStation.java
@@ -0,0 +1,30 @@
+package com.SafetyNet.Safety.model;
+
+public class FireStation {
+    private String address;
+    private int station;
+
+    public FireStation(String address, int station) {
+        this.address = address;
+        this.station = station;
+    }
+
+    public FireStation() {
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getStation() {
+        return station;
+    }
+
+    public void setStation(int station) {
+        this.station = station;
+    }
+}
