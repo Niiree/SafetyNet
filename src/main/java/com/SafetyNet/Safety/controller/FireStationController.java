d82992e87e82139ecc0f5bbe7ba6cb6d9d6196d6
Nicolas.le-stunff <nicolas.le-stunff@hotmail.fr>
2021-06-24 16:48:16 +0200

    Ajouts urls Firestations?StationNumber

--- src/main/java/com/SafetyNet/Safety/controller/FireStationController.java
+++ src/main/java/com/SafetyNet/Safety/controller/FireStationController.java
@@ -2,12 +2,17 @@ package com.SafetyNet.Safety.controller;
 
 
 import com.SafetyNet.Safety.model.FireStation;
-import com.SafetyNet.Safety.service.FireStationService;
+import com.SafetyNet.Safety.model.Person;
+import com.SafetyNet.Safety.service.*;
 
+import com.SafetyNet.Safety.util.ServiceDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 
 //TODO Refactor à faire concernant l'id
@@ -16,27 +21,78 @@ public class FireStationController {
 
     @Autowired
     private FireStationService fireStationService;
+    private PersonService personService = new PersonService();
+    private ServiceDate serviceDate = new ServiceDate();
+
 
     /*
-    Récuperation d'une liste de FireStations
-     */
-    @GetMapping(value = "/firestationInfo")
-    public List<FireStation> listeFireStation(){ return fireStationService.findAll(); }
-
+    * Save Firestation
+    */
     @PostMapping(value = "/firestation")
     public void firestationPost(@RequestBody FireStation fireStation){
         fireStationService.save(fireStation);
     }
+
+    /*
+    * Put Firestation
+    */
     @PutMapping(value = "/firestation/{id}")
     public void firestationPut(@RequestBody FireStation firestation, @PathVariable int id){
-  //      fireStationService.updateFireStation(id);
+        //      fireStationService.updateFireStation(id);
+        //TODO A FINIR
     }
+    /*
+    * Delete Firestation
+    */
     @DeleteMapping(value = "/firestation")
     public void firestationDelete(@RequestBody FireStation firestation){
         fireStationService.remove(firestation);
     }
 
+    /*
+    * URl retourne une liste des personnes par caserne de pompiers correspondantes
+    */
+    @GetMapping(value = "/firestation")
+    public List<String> firestation(@RequestParam int stationNumber ){
+
+        FireStation firestation = fireStationService.findById(stationNumber);
+        List<Person> person = personService.findAll();
+        AtomicInteger adulte = new AtomicInteger();
+        AtomicInteger child = new AtomicInteger();
+
+        List<Person> personFirestation = person.stream()
+                .filter(persons -> firestation.getAddress().contains(persons.getAddress()))
+                .collect(Collectors.toList());
+    
+        List<String> result = new ArrayList<>();
+        
+        for (Person p:personFirestation
+             ) {
+                result.add(p.getLastName()+" "+p.getFirstName()+" "+p.getAddress()+" "+p.getPhone());
+                if(serviceDate.isAdult(p.getBirthdate())) {
+                    adulte.getAndIncrement();
+                }else{
+                    child.getAndIncrement();
+                }
+
+        }
+        result.add(adulte + " Adultes");
+        result.add(child + " enfants");
+        return result;
+
+    }
 
 
-    public void saveFireStation(FireStation fireStation){ fireStationService.save(fireStation); }
+
+
+
+    /*
+    * Récuperation d'une liste de FireStations
+    */
+    @GetMapping(value = "/firestationAll")
+    public List<FireStation> listeFireStation(){
+        return fireStationService.findAll();
+    }
+
+
 }
