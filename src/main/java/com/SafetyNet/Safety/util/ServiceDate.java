3e2c273a30d58517672625295b916f4edfdf45c3
Nicolas.le-stunff <nicolas.le-stunff@hotmail.fr>
2021-06-24 16:47:13 +0200

    ServiceDate

---
+++ src/main/java/com/SafetyNet/Safety/util/ServiceDate.java
@@ -0,0 +1,19 @@
+package com.SafetyNet.Safety.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ServiceDate {
+
+
+    public boolean isAdult(Date date){
+        Date dateNow = new Date(System.currentTimeMillis());
+        SimpleDateFormat simpleDateNow = new SimpleDateFormat("dd/MM/yyyy");
+        long between = dateNow.getTime() - date.getTime();
+
+        if (between/(1000*60*60*24) > 6570 ){
+            return true;
+        }
+        return false;
+    }
+}
