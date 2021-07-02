11fa6614b4ad17cdce9c94c7569a0d37c925b9fc
Nicolas.le-stunff <nicolas.le-stunff@hotmail.fr>
2021-07-02 15:28:32 +0200

    Ajout du swagger

--- src/main/java/com/SafetyNet/Safety/SafetyApplication.java
+++ src/main/java/com/SafetyNet/Safety/SafetyApplication.java
@@ -3,13 +3,14 @@ package com.SafetyNet.Safety;
 import com.SafetyNet.Safety.util.ImportData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.io.IOException;
 
 @SpringBootApplication
+@EnableOpenApi
 public class SafetyApplication {
-
-	//private static ImportData importData;
 
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(SafetyApplication.class, args);
