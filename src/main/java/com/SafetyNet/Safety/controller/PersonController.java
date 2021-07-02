81869231155e28a9bc39b13cebb4358d48cee87a
Nicolas.le-stunff <nicolas.le-stunff@hotmail.fr>
2021-07-02 15:28:55 +0200

    update

--- src/main/java/com/SafetyNet/Safety/controller/PersonController.java
+++ src/main/java/com/SafetyNet/Safety/controller/PersonController.java
@@ -70,7 +70,7 @@ public class PersonController {
 
     /*
      * URl doit retourner le nom, l'adresse, l'âge, l'adresse mail et les antécédents médicaux (médicaments,posologie, allergies) de chaque habitant. Si plusieurs personnes portent le même nom, elles doiventtoutes apparaître
-     * URL OK 
+     * URL OK
      */
     @GetMapping (value = "/communityEmail")
     public List<String> communityEmail(@RequestParam String city){
